3e449e878a8cb58de1504a66474b5e69354b08e6
Macy Lau <macylau94@gmail.com>
2024-09-23 16:20:45 -0600

    set MINE type

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-// var myLink = document.querySelector("#ar-link");
-// if(myLink.relList.supports("ar")) {
-//     myLink.computedStyleMap.display = "block";
-// } else {
-//     myLink.style.display = "none";
-// }
+var myLink = document.querySelector("#ar-link");
+if(myLink.relList.supports("ar")) {
+    myLink.computedStyleMap.display = "block";
+} else {
+    myLink.style.display = "none";
+}
 
-document.getElementById('view3d').addEventListener('click', function() {
-    window.location.href = 'images/Bicycle.usdz';
-});
+// document.getElementById('view3d').addEventListener('click', function() {
+//     window.location.href = 'images/Bicycle.usdz';
+// });
