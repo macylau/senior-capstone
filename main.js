91a8e421027c914cbefabc9d430513a047324c41
Macy Lau <macylau94@gmail.com>
2024-10-07 17:05:18 -0600

    try a different file

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var myLink = document.querySelector("#ar-link");
-if(myLink.relList.supports("ar")) {
-    myLink.computedStyleMap.display = "block";
-} else {
-    myLink.style.display = "none";
-}
+// var myLink = document.querySelector("#ar-link");
+// if(myLink.relList.supports("ar")) {
+//     myLink.computedStyleMap.display = "block";
+// } else {
+//     myLink.style.display = "none";
+// }
 
 // document.getElementById('view3d').addEventListener('click', function() {
 //     window.location.href = 'images/Bicycle.usdz';
